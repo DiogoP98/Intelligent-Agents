efa01b54fda7841b1c4920d8746fb883b19dde79
Diogo Pereira <diogo99p@gmail.com>
2019-12-05 19:22:18 +0000

    LinearProgramming done

--- src/group13/UncertaintyModelling.java
+++ src/group13/UncertaintyModelling.java
@@ -3,30 +3,26 @@ package group13;
 import genius.core.Bid;
 import genius.core.Domain;
 import genius.core.issue.Issue;
+import genius.core.issue.IssueDiscrete;
 import genius.core.issue.ValueDiscrete;
 import genius.core.uncertainty.AdditiveUtilitySpaceFactory;
 import genius.core.uncertainty.BidRanking;
 import genius.core.utility.AdditiveUtilitySpace;
 import genius.core.utility.Evaluator;
-import org.chocosolver.solver.Model;
-import org.chocosolver.solver.Solution;
-import org.chocosolver.solver.Solver;
-import org.chocosolver.solver.constraints.Constraint;
-import org.chocosolver.solver.variables.RealVar;
-import org.chocosolver.solver.variables.IntVar;
+import org.apache.commons.math3.optimization.PointValuePair;
+import org.apache.commons.math3.optimization.linear.*;
+import org.apache.commons.math3.optimization.GoalType;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+
+@SuppressWarnings("deprecation")
 
 public class UncertaintyModelling extends AdditiveUtilitySpaceFactory {
     private final Domain domain;
     private HashMap<Integer, Integer> mapping_issues = new HashMap<>();
-    private HashMap<String, Integer> mapping_z = new HashMap<String, Integer>();
+    HashMap<Integer, Integer> number_values_per_issue = new HashMap<>();
 
-    private RealVar[] issueWeights;
-    private RealVar[] zvars;
-
+    AdditiveUtilitySpace u;
     /**
      * Generates an simple Utility Space on the domain, with equal weights and zero values.
      * Everything is zero-filled to already have all keys contained in the utility maps.
@@ -42,108 +38,125 @@ public class UncertaintyModelling extends AdditiveUtilitySpaceFactory {
         double points = 0;
 
         List<Bid> Bids = bids.getBidOrder();
+        double lowerUtility = bids.getLowUtility();
+        double higherUtility = bids.getHighUtility();
+        estimateUsingBidRanks(bids);
 
-        for (Bid b : Bids) {
-            List<Issue> issues = b.getIssues();
-            for (Issue i : issues) {
-                int no = i.getNumber();
-                ValueDiscrete v = (ValueDiscrete) b.getValue(no);
-                double oldUtil = getUtility(i, v);
-                setUtility(i, v, oldUtil + points);
-            }
-            points++;
-        }
 
-        LP(Bids);
+        LP(Bids, lowerUtility, higherUtility);
 
     }
 
-    private void LP(List<Bid> Bids) {
-        AdditiveUtilitySpace u = getUtilitySpace();
+    private void LP(List<Bid> Bids, double lowerUtility, double higherUtility) {
+        this.u = getUtilitySpace();
 
-        Model model = new Model();
+        List<Issue> issues = this.getDomain().getIssues();
+
+        int vars = 0;
         int key = 0;
-
-        issueWeights = new RealVar[getDomain().getIssues().size()];
-        zvars = new RealVar[Bids.size() - 1];
-
-        List<Issue> issue = this.getDomain().getIssues();
-
-        for(Issue i : issue) {
-            int issueNumber = i.getNumber();
-            issueWeights[key] = model.realVar("I"+ String.valueOf(key), 0.0, 1.0,0.0005d);
+        for(Issue issue: issues) {
+            int issueNumber = issue.getNumber();
+            IssueDiscrete issueD = (IssueDiscrete) issue;
+            int nvalues_issue = issueD.getValues().size();
+            this.number_values_per_issue.put(key,nvalues_issue);
             mapping_issues.put(issueNumber, key++);
+            vars += nvalues_issue;
         }
 
-        key = 0;
-        for(int i = 0; i < Bids.size() - 1; i++)
-            zvars[key] = model.realVar("I"+ String.valueOf(key), 0.0, 1.0,0.0005d);
+        //add the number of slack variables
+        int slackvars = Bids.size() - 1;
+        vars += slackvars;
 
-        double [] previous_values = new double[this.domain.getIssues().size()];
-        double [] values_issue = new double[this.domain.getIssues().size()];
-        double [] weights = new double[this.domain.getIssues().size()];
+        double[] variables = new double[vars];
+
+        for(int i = 0; i < slackvars; i++) {
+            variables[i] = 1;
+        }
+
+        LinearObjectiveFunction objectiveFunction = new LinearObjectiveFunction(variables, 0);
+
+        Collection<LinearConstraint> constraints = new ArrayList<>();
+        for(int pos = 0; pos < vars; pos++) {
+            Arrays.fill(variables, 0);
+            variables[pos] = 1;
+            constraints.add(new LinearConstraint(variables, Relationship.GEQ, 0));
+        }
+        Arrays.fill(variables, 0);
+
+        double [] previous_values = new double[vars];
         int k = 0;
 
         for(Bid b: Bids) {
-            int j = 0;
-            for(Issue i: b.getIssues()) {
-                Evaluator evaluator = u.getEvaluator(i);
-                int issueNumber = i.getNumber();
-                double value = evaluator.getEvaluation(u, b, i.getNumber());
-                values_issue[mapping_issues.get(issueNumber)] = value;
+            double [] values_issue = get_values_bid(b, slackvars, vars);
+
+            //constraint the lowest value bid to have the same value as the given one
+            if(k == 0) {
+                constraints.add(new LinearConstraint(values_issue, Relationship.EQ, lowerUtility));
+            }
+            else {
+                for(int pos = 0; pos < vars; pos++)
+                    variables[pos] = values_issue[pos] - previous_values[pos];
+
+                //add correspondent slack variable
+                variables[k-1] = 1;
+                constraints.add(new LinearConstraint(variables, Relationship.GEQ, 0));
             }
 
-            if(k > 0) {
-                for(Issue i: issue) {
-                    int issueNumber = i.getNumber();
-                    Integer map_key = mapping_issues.get(issueNumber);
-                    weights[map_key] = values_issue[map_key] - previous_values[map_key];
-                }
+            //constraint the biggest value bid to have the same value as the given one
+            if(k == slackvars)
+                constraints.add(new LinearConstraint(values_issue, Relationship.EQ, higherUtility));
 
-                model.post(new Constraint("MyConstraint" + String.valueOf(k-1), new MyPropagator(issueWeights, weights, 0.0)));
+            k += 1;
 
-            }
-
-            if(k == Bids.size() - 1)
-                model.post(new Constraint("MyConstraint" + String.valueOf(k), new MyPropagator2(issueWeights, values_issue, 1.0)));
-
-            k++;
-
-            for(int c = 0; c < values_issue.length; c++)
-                previous_values[c] = values_issue[c];
-
+            for(int pos = 0; pos < vars; pos++)
+                previous_values[pos] = values_issue[pos];
         }
 
-        Solver solver = model.getSolver();
-        Solution solution = solver.findSolution();
-        /*for(Issue i : issue) {
-            int issueNumber = i.getNumber();
-            //u.setWeight(i,(double) solution.getRealBounds(issueWeights[mapping_issues.get(issueNumber)])[0]);
-            System.out.println(Arrays.toString(solution.getRealBounds(issueWeights[mapping_issues.get(issueNumber)])));
-        }*/
+        SimplexSolver solver = new SimplexSolver();
+        solver.setMaxIterations(Integer.MAX_VALUE);
+        PointValuePair solution = solver.optimize(objectiveFunction, constraints, GoalType.MINIMIZE, true);
 
-        System.out.println(solution);
+        int pos = slackvars;
+        for(Issue issue: issues) {
+            int issueNumber = issue.getNumber();
+            IssueDiscrete issueD = (IssueDiscrete) issue;
+            for(ValueDiscrete valueD: issueD.getValues()) {
+                setUtility(issue, valueD, solution.getPoint()[pos]);
+                pos+=1;
+            }
+        }
+
+        normalizeWeights();
     }
 
-    public void estimateUsingBidRanks_(BidRanking bids) {
-        double [] weights;
-        int issuesIndex = 0;
-        HashMap<Integer, String> issuesMap = new HashMap<>();
-        double points = 0;
-        for (Bid b: bids.getBidOrder()) {
-            List<Issue> issues = b.getIssues();
-            for(Issue i : issues){
-                int no = i.getNumber();
-                ValueDiscrete v = (ValueDiscrete) b.getValue(no);
-                double oldUtil = getUtility(i,v);
-                setUtility(i, v, oldUtil + points);
-                if(!issuesMap.containsKey(no)){
-                    issuesMap.put(no, Integer.toString(issuesIndex));
+    private double[] get_values_bid(Bid b, int numberSlacks, int arraySize) {
+        double [] values = new double[arraySize];
+
+        for(Issue issue: b.getIssues()) {
+            Evaluator evaluator = this.u.getEvaluator(issue);
+            int issueNumber = issue.getNumber();
+            IssueDiscrete issueD = (IssueDiscrete) issue;
+
+            ValueDiscrete v = (ValueDiscrete) b.getValue(issueNumber);
+            double value = evaluator.getEvaluation(u, b, issueNumber);
+
+            int index = numberSlacks;
+            int key = mapping_issues.get(issueNumber);
+            //get the number of issues before this, and respective number of values
+            //values[key] = value;
+            for(int issues = 0; issues < key; issues++)
+                index += this.number_values_per_issue.get(issues);
+
+            for(ValueDiscrete valueD: issueD.getValues()) {
+                if(valueD.hashCode() == v.hashCode()) {
+                    values[index] = 1;
+                    break;
                 }
+                else
+                    index += 1;
             }
-            points =+ 1;
         }
 
-
+        return values;
     }
 }
