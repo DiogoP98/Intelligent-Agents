2c42f3cbf72646861e2499cc1fd85331a821b5ae
Diogo Pereira <diogo99p@gmail.com>
2019-12-07 00:12:40 +0000

    created heuristic for uncertainty modelling

--- src/group13/UncertaintyModelling.java
+++ src/group13/UncertaintyModelling.java
@@ -8,7 +8,6 @@ import genius.core.issue.ValueDiscrete;
 import genius.core.uncertainty.AdditiveUtilitySpaceFactory;
 import genius.core.uncertainty.BidRanking;
 import genius.core.utility.AdditiveUtilitySpace;
-import genius.core.utility.Evaluator;
 import org.apache.commons.math3.optimization.PointValuePair;
 import org.apache.commons.math3.optimization.linear.*;
 import org.apache.commons.math3.optimization.GoalType;
@@ -19,11 +18,17 @@ import java.util.*;
 
 public class UncertaintyModelling extends AdditiveUtilitySpaceFactory {
     private final Domain domain;
-    private HashMap<Integer, Integer> mapping_issues = new HashMap<>();
-    HashMap<Integer, Integer> number_values_per_issue = new HashMap<>();
-    int threshold;
+    private int threshold;
+    public AdditiveUtilitySpace u;
 
-    AdditiveUtilitySpace u;
+    private HashMap<Integer, Integer> mapping_issues = new HashMap<>();
+    private HashMap<Integer, Integer> number_values_per_issue = new HashMap<>();
+
+    private HashMap<String, Integer> mapping_values = new HashMap<String, Integer>();
+
+    ArrayList<Integer>[][] positions;
+    Double [][] means;
+
     /**
      * Generates an simple Utility Space on the domain, with equal weights and zero values.
      * Everything is zero-filled to already have all keys contained in the utility maps.
@@ -37,13 +42,13 @@ public class UncertaintyModelling extends AdditiveUtilitySpaceFactory {
     }
 
     public void UncertaintyEstimation(BidRanking bids) {
-        double points = 0;
-
         List<Bid> Bids = bids.getBidOrder();
         double lowerUtility = bids.getLowUtility();
         double higherUtility = bids.getHighUtility();
 
         List<Issue> issues = this.getDomain().getIssues();
+        this.positions = new ArrayList[this.getDomain().getIssues().size()][];
+        this.means = new Double[this.getDomain().getIssues().size()][];
 
         int vars = 0;
         int key = 0;
@@ -51,6 +56,18 @@ public class UncertaintyModelling extends AdditiveUtilitySpaceFactory {
             int issueNumber = issue.getNumber();
             IssueDiscrete issueD = (IssueDiscrete) issue;
             int nvalues_issue = issueD.getValues().size();
+            this.positions[key] = new ArrayList[nvalues_issue];
+            this.means[key] = new Double[nvalues_issue];
+
+            int count_values = 0;
+            for (ValueDiscrete valueDiscrete : issueD.getValues()) {
+                this.mapping_values.put(valueDiscrete.toString()+ String.valueOf(issueNumber), count_values);
+
+                this.positions[key][count_values] = new ArrayList<>();
+
+                count_values += 1;
+            }
+
             this.number_values_per_issue.put(key,nvalues_issue);
             this.mapping_issues.put(issueNumber, key++);
             vars += nvalues_issue;
@@ -64,18 +81,49 @@ public class UncertaintyModelling extends AdditiveUtilitySpaceFactory {
     }
 
     private void heuristic_calculation(List<Bid> Bids) {
-        double points = 0;
-        for (Bid b : Bids){
-            List<Issue> issues = b.getIssues();
-            for (Issue i : issues){
-                int no = i.getNumber();
-                ValueDiscrete v = (ValueDiscrete) b.getValue(no);
-                double oldUtil = getUtility(i, v);
-                setUtility(i, v, oldUtil + points);
+        int points = 1;
+
+        for(Bid b: Bids) {
+            for(Issue issue: b.getIssues()) {
+                int issueNumber = issue.getNumber();
+                ValueDiscrete value = (ValueDiscrete) b.getValue(issueNumber);
+
+                this.positions[mapping_issues.get(issueNumber)][mapping_values.get(value + String.valueOf(issueNumber))].add(points);
             }
-            points++;
+
+            points += 1;
         }
-        normalizeWeights();
+
+        for(Issue issue: this.getDomain().getIssues()) {
+            int issueNumber = issue.getNumber();
+            IssueDiscrete issueD = (IssueDiscrete) issue;
+            int key = mapping_issues.get(issueNumber);
+
+            double max_deviation = 0;
+
+            int value_number = 0;
+            for (ValueDiscrete valueDiscrete : issueD.getValues()) {
+                double total = 0;
+                double size = (double) this.positions[key][mapping_values.get(valueDiscrete + String.valueOf(issueNumber))].size();
+                List<Integer> value_position = this.positions[key][mapping_values.get(valueDiscrete + String.valueOf(issueNumber))];
+
+                for(Integer pos: value_position)
+                    total += pos;
+
+                double mean = total / size;
+                double deviation = 0;
+                for(Integer pos: value_position)
+                    deviation += (mean - pos)*(mean - pos);
+
+                deviation = deviation / size;
+                max_deviation = Math.max(deviation, max_deviation);
+                this.setUtility(issue, valueDiscrete, mean);
+            }
+
+            this.setWeight(issue, max_deviation);
+        }
+
+        this.normalizeWeights();
     }
 
     private void LP(List<Bid> Bids, int vars, double lowerUtility, double higherUtility, List<Issue> issues) {
@@ -137,19 +185,18 @@ public class UncertaintyModelling extends AdditiveUtilitySpaceFactory {
         for(Issue issue: issues) {
             IssueDiscrete issueD = (IssueDiscrete) issue;
             for(ValueDiscrete valueD: issueD.getValues()) {
-                setUtility(issue, valueD, solution.getPoint()[pos]);
+                this.setUtility(issue, valueD, solution.getPoint()[pos]);
                 pos+=1;
             }
         }
 
-        normalizeWeights();
+        this.normalizeWeights();
     }
 
     private double[] get_values_bid(Bid b, int numberSlacks, int arraySize) {
         double [] values = new double[arraySize];
 
         for(Issue issue: b.getIssues()) {
-            Evaluator evaluator = this.u.getEvaluator(issue);
             int issueNumber = issue.getNumber();
             IssueDiscrete issueD = (IssueDiscrete) issue;
 
