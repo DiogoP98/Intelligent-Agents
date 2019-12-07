88b95c8c9c7a933fc5268ce0549f8520ee2e72a8
subash774 <subash.poudyal8@gmail.com>
2019-12-07 19:22:01 +0000

    fixed index out of error, agent now performing better

--- src/group13/Agent13.java
+++ src/group13/Agent13.java
@@ -15,9 +15,8 @@ import genius.core.parties.AbstractNegotiationParty;
 import genius.core.parties.NegotiationInfo;
 import genius.core.uncertainty.BidRanking;
 import genius.core.uncertainty.ExperimentalUserModel;
-import genius.core.utility.AdditiveUtilitySpace;
 import genius.core.utility.UncertainAdditiveUtilitySpace;
-
+import genius.core.analysis.MultilateralAnalysis;
 /**
  * ExampleAgent returns the bid that maximizes its own utility for half of the negotiation session.
  * In the second half, it offers a random bid. It only accepts the bid on the table in this phase,
@@ -31,12 +30,14 @@ public class Agent13 extends AbstractNegotiationParty {
     private Bid myLastOffer;
     private OpponentModel opponent;
     private final Map<AgentID, OpponentModel> opponentsModels = new HashMap<>();
-    private double concessionRate = 1;
+    private double concessionRate = 0.1;
     private final Random randomGenerator = new Random();
     private UncertaintyModelling factory;
     ExperimentalUserModel e = ( ExperimentalUserModel ) userModel ;
     UncertainAdditiveUtilitySpace realUSpace;
-    private List<Double> bidHistory = new ArrayList<>();
+    private List<Double> bidHistoryAgent = new ArrayList<>();
+    private List<Double> bidHistoryOpponent = new ArrayList<>();
+
 
     @Override
     public void init(NegotiationInfo info) {
@@ -44,11 +45,10 @@ public class Agent13 extends AbstractNegotiationParty {
 
         System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
         Domain domain = getDomain();
-
+        this.opponent = new OpponentModel(domain);
         if (hasPreferenceUncertainty()) {
             this.factory = new UncertaintyModelling(domain);
             //realUSpace = e. getRealUtilitySpace();
-            this.opponent = new OpponentModel(domain);
 
             BidRanking bidRanking = userModel.getBidRanking();
             Bid worstBid = bidRanking.getMinimalBid();
@@ -88,6 +88,7 @@ public class Agent13 extends AbstractNegotiationParty {
                 if(!result.contains(randomBid)){
                     deadLimit = -1;
                 }
+
                 result.add(randomBid);
             }
             count++;
@@ -107,7 +108,8 @@ public class Agent13 extends AbstractNegotiationParty {
      */
     @Override
     public Action chooseAction(List<Class<? extends Action>> list) {
-        List <Double> lastNBids;
+        List <Double> lastNAgentBids;
+        List <Double> lastNOpponentBids;
         int n = 30;
         // According to Stacked Alternating Offers Protocol list includes
         // Accept, Offer and EndNegotiation actions only.
@@ -116,25 +118,38 @@ public class Agent13 extends AbstractNegotiationParty {
         // concerned with the actual internal clock.
 
         // First half of the negotiation offering the max utility (the best agreement possible) for Example Agent
-        if (time < 0.1) {
+        if (time < 0.01) {
             return new Offer(this.getPartyId(), this.getMaxUtilityBid());
         } else {
-            if (time > 0.3) concessionRate = 0.17;
-            if (time > 0.9) concessionRate = 0.27;
-            if (time > 0.95) concessionRate = 0.32;
+//            if (time > 0.3) concessionRate = 0.1;
+//            if (time > 0.9) concessionRate = 0.20;
+//            if (time > 0.95) concessionRate = 0.27;
 
             double utilityThreshold = getUtilityThreshold();
 
             if(time > 0.9 && lastReceivedOffer != null){
-                if(bidHistory.size() > n){
-                    lastNBids = bidHistory.subList(bidHistory.size() - n, bidHistory.size() - 2);
-                } else {
-                    lastNBids = bidHistory.subList(0, bidHistory.size() - 2);
+                try {
+                    if(bidHistoryAgent.size() > n){
+                        lastNAgentBids = bidHistoryAgent.subList(bidHistoryAgent.size() - n, bidHistoryAgent.size() - 2);
+                        lastNOpponentBids = bidHistoryOpponent.subList(bidHistoryOpponent.size() - n, bidHistoryOpponent.size() - 2);
+                    }
+                    else{
+                        lastNAgentBids = bidHistoryAgent.subList(0, bidHistoryAgent.size() - 2);
+                        lastNOpponentBids = bidHistoryOpponent.subList(0, bidHistoryOpponent.size() - 2);
+                    }
+                } catch (Exception e){
+                    lastNAgentBids = bidHistoryAgent;
+                    lastNOpponentBids = bidHistoryOpponent;
                 }
 
-                boolean sudden = suddenOpponentConcession(lastNBids, this.utilitySpace.getUtility(lastReceivedOffer));
+                boolean suddenAgentIncrease = detectSuddenChange(lastNAgentBids, this.utilitySpace.getUtility(lastReceivedOffer), 3);
+                boolean suddenOpponentIncrease = detectSuddenChange(lastNOpponentBids, this.opponent.getValue(lastReceivedOffer), 3);
 
-                if(sudden && this.utilitySpace.getUtility(lastReceivedOffer) >= utilityThreshold){
+                if(suddenAgentIncrease && this.utilitySpace.getUtility(lastReceivedOffer) >= utilityThreshold){
+                    System.out.println("We gained!");
+                    return new Accept(this.getPartyId(), lastReceivedOffer);
+                }
+                if(suddenOpponentIncrease && this.opponent.getValue(lastReceivedOffer) >= utilityThreshold){
                     System.out.println("THEY DROPPED!");
                     return new Accept(this.getPartyId(), lastReceivedOffer);
                 }
@@ -150,15 +165,16 @@ public class Agent13 extends AbstractNegotiationParty {
             }
 
 
-
             // Generate random bids above threshold
-            Set<Bid> bidSet = this.generateBids(utilityThreshold, 500, 20000);
+            Set<Bid> bidSet = this.generateBids(utilityThreshold, 5000, 30000);
 
-            if(randomGenerator.nextDouble() <= 0.03) { // randomly bid .5% chance of doing that
-                return new Offer(this.getPartyId(), pickRandomBid(bidSet));
+            if(randomGenerator.nextDouble() <= 0.01) {
+                this.myLastOffer =  pickRandomBid(bidSet);
+                return new Offer(this.getPartyId(),this.myLastOffer);
             } else {
                 // java did some weird shit to it, it's basically saying from bidset, compare and get the best one
                 Bid bestBid = Collections.max(bidSet, Comparator.comparingDouble(this::getOpponentScore));
+                this.myLastOffer = bestBid;
                 return new Offer(this.getPartyId(), bestBid);
             }
         }
@@ -178,10 +194,12 @@ public class Agent13 extends AbstractNegotiationParty {
 
             opponentsModels.putIfAbsent(sender, new OpponentModel(getDomain()));
             opponentsModels.get(sender).updateFrequency(offer.getBid());
+            this.opponent.updateFrequency(offer.getBid());
             // storing last received offer
             lastReceivedOffer = offer.getBid();
-            if(getTimeLine().getTime() > 0.85){
-                bidHistory.add(this.utilitySpace.getUtility(lastReceivedOffer));
+            if(getTimeLine().getTime() > 0.87){
+                bidHistoryAgent.add(this.utilitySpace.getUtility(lastReceivedOffer));
+                bidHistoryOpponent.add(opponent.getValue(lastReceivedOffer));
             }
 
         }
@@ -224,10 +242,10 @@ public class Agent13 extends AbstractNegotiationParty {
     }
 
 
-    public boolean suddenOpponentConcession(List<Double> arr, double val){
+    public boolean detectSuddenChange(List<Double> arr, double val, int n){
         double [] std = getStandardDeviation(arr);
-        double sig3 = std[1] * 3; // std * 3
-        return val > (sig3 + std[0]); // 3sig + mean
+        double sigN = std[1] * n; // std * n
+        return val > (sigN + std[0]); // 3sig + mean
     }
 
     public Bid pickRandomBid(Set<Bid> bidSet){
